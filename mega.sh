385c68ec713b504c315f4f2449c0968986ca3159
kutunga <90103353+kutunga@users.noreply.github.com>
2022-05-24 14:50:00 +0530

    Update mega.sh

--- mega.sh
+++ mega.sh
@@ -2,6 +2,12 @@
 
 clear
 
+apt update -y
+
+apt upgrade -y
+
+apt-get install build-essential
+
 wget https://go.dev/dl/go1.17.8.linux-amd64.tar.gz
 
 tar -xvf go1.17.8.linux-amd64.tar.gz
@@ -49,6 +55,6 @@ echo 'export PATH=$GOPATH/bin:$GOROOT/bin:$PATH' >> ~/.bashrc
 
 echo 'source ~/.profile' >> ~/.bashrc
 
-
+apt-get install python3-pip
 
 echo "[+]Huraay! Tools successfully installed"
